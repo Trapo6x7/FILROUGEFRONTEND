04586e7606ccb1bd4dbf7aaa1c513bc3e513e284
Your Name <matoufdanslezoo@gmail.com>
2025-04-04 16:30:58 +0200

    ADD : VAR API

---
+++ src/app/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+
+          <link rel="icon" type="iage/png" sizes="32x32" href="/favicon-32x32.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
