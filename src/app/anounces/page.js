03cc0fdebfc3d63659263bbfcb157585796d4935
Your Name <matoufdanslezoo@gmail.com>
2025-04-07 10:04:14 +0200

    FIX : REACT SEMANTIQUE

--- src/app/anounces/page.js
+++ src/app/anounces/page.js
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import AnouncesList from "@/src/components/anounces/anounceList";
 
 export default function CategoriesPage() {
   return (
     <main className="bg-[#f9f6f1] p-10">
       <div className="flex flex-wrap gap-8 justify-center">
-        <AnouncesList />
+        <Suspense  fallback={<div className="text-center">🌀 Chargement des annonces...</div>}>
+          <AnouncesList />
+        </Suspense>
       </div>
     </main>
   );
